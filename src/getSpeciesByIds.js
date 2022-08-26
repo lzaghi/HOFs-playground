362d29b9594b2121e9a70e3318dc6f4f20f5cb11
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-26 15:15:11 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // a
+function getSpeciesByIds(...ids) {
+  const array = [];
+  ids.forEach((id) =>
+    data.species.forEach((objeto) => {
+      if (objeto.id === id) array.push(objeto);
+    }));
+
+  return array;
 }
 
 module.exports = getSpeciesByIds;
