a09263cad8b0979035e65710d500bbdd4f75b30b
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-26 15:34:56 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let especie = {};
+  data.species.forEach((objeto) => {
+    if (objeto.name === animal) especie = objeto;
+  });
+  return especie.residents.every((pet) => pet.age >= 7);
 }
 
 module.exports = getAnimalsOlderThan;
