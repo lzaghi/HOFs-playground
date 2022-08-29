5479c64d6fcdfb8507ad38bfd791bbfb692b4ec6
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-29 18:08:28 -0300

    requisito 4 quase completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const arrayDeArrays = data.employees.map((element) => element.managers);
+  const array = arrayDeArrays.reduce((acc, curr) => acc.concat(curr), []);
+  return array.some((codigo) => codigo === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager() === true) {
+    const lista = [];
+    data.employees.forEach((pessoa) => {
+      if (pessoa.managers.includes(managerId)) {
+        lista.push(`${pessoa.firstName} ${pessoa.lastName}`);
+      }
+    });
+    return lista;
+  }
+  if (isManager() === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
